619b5610564f84cd01ac8b7220e68d82330017d6
singhsatyendra8052 <singhsatyendra8052@gmail.com>
2024-09-11 01:15:06 +0530

    signup and logout api

--- backend/Controller/index.js
+++ backend/Controller/index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { Router } from "express";
-import { signup } from "../Routes/manufacture.js";
+import { signup, login } from "../Routes/manufacture.js";
+import multer from "multer";
+const app = express();
+const upload = multer();
 const router = Router();
 
-router.post("/signup", signup);
+router.post("/signup", upload.none(), signup);
+router.post("/login", upload.none(), login);
 
 export default router;
