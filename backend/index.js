15b67bceabecfe78bd148567a1734fecd2d30322
Satyam Kumar <sk9161511745@gmail.com>
2024-09-10 23:55:43 +0530

    model is ready

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,23 @@
-console.log('Hello from the backend!');
+import  dotenv  from 'dotenv';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+dotenv.config();
+const Port = process.env.PORT || 8000;
+
+//regular express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//cookie parser middleware
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(Port, () => {
+     console.log(`Server is running on http://localhost:${Port}`);
+     }
+);
